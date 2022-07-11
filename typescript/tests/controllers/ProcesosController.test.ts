7b65a40691c6402e52fc87bacdaa77eef50e1f78
José Luis González <joseluisgonsan@gmail.com>
2022-07-11 10:31:05 +0200

    Procesos Controller Mock

--- typescript/tests/controllers/ProcesosController.test.ts
+++ typescript/tests/controllers/ProcesosController.test.ts
@@ -1,98 +1,116 @@
 import { describe, test, assert, beforeEach, afterEach, expect, vi } from 'vitest'
 import Proceso from '../../src/models/Proceso'
 import ProcesosController from '../../src/controllers/ProcesosController'
-import ColaPrioritariaImp from '../../src/repositories/ColaPrioritariaImp'
+import ColaPrioritaria from '../../src/repositories/ColaPrioritaria'
 import ProcesosException from '../../src/errors/ProcesosException'
+// import ColaPrioritariaImp from '../../src/repositories/ColaPrioritariaImp'
 
-// Lo voy a hacer sin Mocks por ahora
-// Describimos la suite
-describe('Suite de test de Controlador de Procesos', () => {
-  let colaPrioritaria: ColaPrioritariaImp
+// Creamos nuestro objeto de mock completo de Cola Prioritaria
+const ColaPrioritariaImp = vi.fn(() => ({
+  getAll: vi.fn(() => [new Proceso(2, 'proceso 2', 2), new Proceso(1, 'proceso 1', 1)]),
+  size: vi.fn(() => 2),
+  push: vi.fn(),
+  pop: vi.fn(() => new Proceso(1, 'proceso 1', 1)),
+  isEmpty: vi.fn(() => false),
+  getById: vi.fn(() => new Proceso(1, 'proceso 1', 1)),
+}))
+
+// Creamos nuestro objeto de mock completo de Cola Prioritaria con los casos "raros"
+const ColaPrioritariaImpErr = vi.fn(() => ({
+  getAll: vi.fn(() => []),
+  size: vi.fn(() => 0),
+  push: vi.fn(),
+  pop: vi.fn(() => undefined),
+  isEmpty: vi.fn(() => true),
+  getById: vi.fn(() => undefined),
+}))
+
+describe('Suite de test de Controlador de Procesos Mock Repositorio Cola Prioritaria', () => {
+  let colaPrioritaria: ColaPrioritaria
   let procesosController: ProcesosController
 
-  // En cada test creamos la cola
   beforeEach(() => {
     colaPrioritaria = new ColaPrioritariaImp()
     procesosController = new ProcesosController(colaPrioritaria)
   })
 
   afterEach(() => {
-    // vi.clearAllMocks()
+    vi.clearAllMocks()
   })
 
   test('debería obtener todos los procesos respetando prioridad', () => {
-    const proceso1 = new Proceso(1, 'Proceso 1', 1)
-    const proceso2 = new Proceso(2, 'Proceso 2', 2)
-    colaPrioritaria.push(proceso1)
-    colaPrioritaria.push(proceso2)
-
     const res = procesosController.getAll()
 
-    expect(res).toEqual([proceso2, proceso1])
+    expect(colaPrioritaria.getAll).toBeCalledTimes(1)
+    expect(res.length).toBe(2)
     assert.equal(res.length, 2)
-    assert.equal(res[0], proceso2)
-    assert.equal(res[1], proceso1)
     assert.isTrue(res[0].prioridad >= res[1].prioridad)
   })
 
   test('debería insertar un proceso', () => {
-    const proceso1 = new Proceso(1, 'Proceso 1', 1)
-
-    procesosController.push(proceso1)
-    const res = procesosController.getAll()
-
-    expect(res).toEqual([proceso1])
-    assert.equal(res.length, 1)
-    assert.equal(res[0], proceso1)
-  })
-
-  test('debería obtener un proceso', () => {
-    const proceso1 = new Proceso(1, 'Proceso 1', 1)
+    const proceso1 = new Proceso(1, 'proceso 1', 1)
 
     procesosController.push(proceso1)
     const res = procesosController.getById(1)
 
-    assert.equal(res, proceso1)
+    expect(colaPrioritaria.push).toBeCalledTimes(1)
+    expect(res).toEqual(proceso1)
+    assert.equal(res.id, proceso1.id)
+  })
+
+  test('debería obtener un proceso', () => {
+    const res = procesosController.getById(1)
+
+    expect(colaPrioritaria.getById).toBeCalledTimes(1)
+    expect(colaPrioritaria.getById).toBeCalledWith(1)
+    assert.equal(res.id, 1)
   })
 
   test('debería sacar un proceso', () => {
-    const proceso1 = new Proceso(1, 'Proceso 1', 1)
-
-    procesosController.push(proceso1)
     const res = procesosController.pop()
 
-    assert.equal(res, proceso1)
+    expect(colaPrioritaria.pop).toBeCalledTimes(1)
+    assert.equal(res.id, 1)
   })
 
-  test('debería ser vacía', () => {
+  test('debería saber si la cola está llena', () => {
     const res = procesosController.isEmpty()
 
-    assert.isTrue(res)
-  })
-
-  test('debería no ser vacía', () => {
-    const proceso1 = new Proceso(1, 'Proceso 1', 1)
-
-    procesosController.push(proceso1)
-
-    const res = procesosController.isEmpty()
-
+    expect(colaPrioritaria.isEmpty).toBeCalledTimes(1)
     assert.isFalse(res)
   })
 
+  test('debería saber si la cola está vacía', () => {
+    colaPrioritaria = new ColaPrioritariaImpErr()
+    procesosController = new ProcesosController(colaPrioritaria)
+    const res = procesosController.isEmpty()
+
+    expect(colaPrioritaria.isEmpty).toBeCalledTimes(1)
+    assert.isTrue(res)
+  })
+
   test('debería obtener una excepción si proceso no existe al consultar', () => {
+    colaPrioritaria = new ColaPrioritariaImpErr()
+    procesosController = new ProcesosController(colaPrioritaria)
+
     const res = assert.throws(() => {
       procesosController.getById(1)
     }, ProcesosException) as unknown as ProcesosException
 
+    expect(colaPrioritaria.getById).toBeCalledTimes(1)
+    expect(colaPrioritaria.getById).toBeCalledWith(1)
     assert.equal(res.message, 'No existe el proceso con id 1')
   })
 
   test('debería obtener una excepción si proceso no existe al extraer', () => {
+    colaPrioritaria = new ColaPrioritariaImpErr()
+    procesosController = new ProcesosController(colaPrioritaria)
+
     const res = assert.throws(() => {
       procesosController.pop()
     }, ProcesosException) as unknown as ProcesosException
 
+    expect(colaPrioritaria.pop).toBeCalledTimes(1)
     assert.equal(res.message, 'No existe el procesos o la cola esta vacía')
   })
 })
